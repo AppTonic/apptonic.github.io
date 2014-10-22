e23a2021862418d3ce8eb08451d9ed62c0d0f3b4
Craig Smitham <craigsmitham@gmail.com>
2014-10-22 14:40:33 -0700

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+(function($) {
+
+// prettyPhoto
+	$(document).ready(function(){
+		$('a[data-gal]').each(function() {
+			$(this).attr('rel', $(this).data('gal'));
+		});  	
+		$("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
+	}); 
+
+	$('.enabled-tab').click(function (e) {
+  		e.preventDefault()
+  		$(this).tab('show')
+	})
+		
+})(jQuery);
